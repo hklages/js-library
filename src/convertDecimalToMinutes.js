ba21e263d5e0eeb6f5612b763db95cef919ff78a
hklages <heklaf@gmail.com>
2019-09-18 12:08:54 +0200

    added holiday

--- src/convertDecimalToMinutes.js
+++ src/convertDecimalToMinutes.js
@@ -1,7 +1,7 @@
 function convertDecimalToMinutes (time) {
   // PURPOSE Converts hours/min in decimal to hours with hours:minutes, example 3.5 to 03:30  or 3 to 03:00
   // PREREQ: decimal sign is . (may be omitted if integer)
-  let hrs = parseInt(Number(time));
-  let min = Math.round((Number(time) - hrs) * 60);
+  const hrs = parseInt(Number(time));
+  const min = Math.round((Number(time) - hrs) * 60);
   return pad2digits(hrs) + ':' + pad2digits(min);
 }

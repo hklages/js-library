cd0126cd4b2526253dc1ff3d9a5b10352f206ae3
hklages <heklaf@gmail.com>
2019-09-18 11:59:31 +0200

    sun position seems good solutin

--- src/getIsoTimeZoneTimeV2.js
+++ src/getIsoTimeZoneTimeV2.js
@@ -1,11 +1,16 @@
+/**
+ * Provides current time for a specific timezone in ISO format 2019-07-02T14:59:38+02:00
+ *  Maybe obsolete as there is node-red-contrib-sun-position to provide in exactly that format.
+ *
+ * @since      2019-07-09T1503 V2
+ * @param {string}   timezone timezone identifier, see  https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+ *
+ * @return {string} Return the time in ISO format for that timezone
+ *
+ * Call example: getIsoTimeZoneTime('Europe/Berlin')
+ * Hint: NO milliseconds, string format, takes in account DST (summer/winter time)
+ */
 function getIsoTimeZoneTimeV2(timeZone) {
-  // PURPOSE Provides current date/time for a specific time zone in ISO format
-  // VERSION 2019-07-09T1503 V2
-  // RETURN current time in ISO format with timezone. example 2019-07-02T14:59:38+02:00
-  // INPUT timeZone from list: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
-  // CALL example: getIsoTimeZoneTime('Europe/Berlin')
-  // HINT NO milliseconds, string format, DST conform
-
   // Get timezone short name
   let now = new Date();
   let tz = now.toLocaleDateString('en-US', { timeZone: timeZone, timeZoneName: 'short' });
@@ -18,9 +23,9 @@ function getIsoTimeZoneTimeV2(timeZone) {
     nowTZ = nowTZ.substr(0, nowTZ.length - 5) + 'Z';
   } else {
     // convert timezone from GMT+x to +0x:00 format and offset in hours
-    var offsetMsec = 0; // offset in milliseconds
+    let offsetMsec = 0; // offset in milliseconds
     // get + or -
-    var offsetText = tz.substr(0, 1); // offset as text
+    let offsetText = tz.substr(0, 1); // offset as text
     tz = tz.substr(1);
     offsetMsec = parseFloat(tz) * 3600000;
     if (offsetText === '-') offsetMsec = (-1) * offsetMsec;
